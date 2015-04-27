a6366ebe96c1379e2b9d04b7b58b17c1ae9d5176
liamnichols <liam.nichols.ln@gmail.com>
2015-04-27 20:57:48 +0100

    began to add table datasource

--- BLEScanner/ViewController.m
+++ BLEScanner/ViewController.m
@@ -14,6 +14,7 @@
 @interface ViewController () <BLEPeripheralManagerDelegate>
 
 @property (nonatomic, strong) BLEPeripheralManager *manager;
+@property (nonatomic, strong) NSArray *sections;
 
 @end
 
@@ -22,19 +23,76 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    // create an array of sections that the ui supports
+    self.sections = @[
+        @{
+            @"title" : @"iBeacons",
+            @"class" : [BLEPeripheral class], //TODO: iBeacon Support
+            @"columns" : @[
+                @"uuid",
+                @"rssi"
+            ]
+        },
+        @{
+            @"title" : @"Estimote Stickers",
+            @"class" : [BLEStickerPeripheral class],
+            @"columns" : @[
+                @"identifier",
+                @"rssi"
+            ]
+         }
+    ];
+    
+    // set the sections on the segmented control
+    self.segmentedControl.segmentCount = self.sections.count;
+    [self.sections enumerateObjectsUsingBlock:^(NSDictionary *section, NSUInteger idx, BOOL *stop) {
+        [self.segmentedControl setLabel:section[@"title"] forSegment:idx];
+    }];
 
+    // create an instance of the manager
     self.manager = [BLEPeripheralManager new];
     self.manager.delegate = self;
-    [self.manager registerPeripheralClass:[BLEStickerPeripheral class]];
+    [self.sections enumerateObjectsUsingBlock:^(NSDictionary *section, NSUInteger idx, BOOL *stop) {
+        [self.manager registerPeripheralClass:section[@"class"]];
+    }];
+    
+    // update the tableView based on the default segment index
+    [self segmentedControlSwitched:nil];
 }
+
+- (Class)peripheralClassAtSegmentIndex:(NSInteger)index
+{
+    return self.sections[index][@"class"];
+}
+
+#pragma mark - BLEPeripheralManagerDelegate
 
 - (void)peripheralManager:(BLEPeripheralManager *)manager didUpdatePeripheral:(BLEPeripheral *)peripheral
 {
     NSLog(@"didUpdatePeripheral: %@", peripheral);
 }
 
+#pragma mark - Actions
+
 - (IBAction)segmentedControlSwitched:(id)sender
 {
+    NSDictionary *section = self.sections[self.segmentedControl.selectedSegment];
+    
+    // remove the old table columns
+    [[self.tableView.tableColumns copy] enumerateObjectsUsingBlock:^(NSTableColumn *column, NSUInteger idx, BOOL *stop) {
+        [self.tableView removeTableColumn:column];
+    }];
+    
+    // add the new columns
+    [section[@"columns"] enumerateObjectsUsingBlock:^(NSString *identifier, NSUInteger idx, BOOL *stop) {
+        NSTableColumn *column = [[NSTableColumn alloc] initWithIdentifier:identifier];
+        column.title = identifier;
+        [self.tableView addTableColumn:column];
+    }];
+    
+    // reload the tableView
+    [self.tableView reloadData];
 }
 
 - (IBAction)scanButtonPressed:(id)sender
